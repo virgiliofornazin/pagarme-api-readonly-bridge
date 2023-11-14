174eca13c5694aa5817042997105058114e4f7e0
Virgilio A. Fornazin <virgiliofornazin@gmail.com>
2023-11-14 13:15:32 +0000

    updates sent

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 from typing import Optional
 import os
 # from dotenv import load_dotenv
+
 # load_dotenv()
+
 
 BASE_URL = "https://api.pagar.me/core/v5/"
 
@@ -15,24 +17,28 @@ def get_data_from_page(url, head):
 
 def get_all_pages_data(initial_url, head, filters=None):
     all_data = []
-    if filters:
-        current_url = f"{initial_url}?page=1&size=200&navigation=True&{filters}"
-    else: current_url = f"{initial_url}?page=1&size=200&navigation=True"
- 
-    next_page_url = initial_url  # Initialize next_page_url with the initial_url
-   
-    while next_page_url:
-        response_data = get_data_from_page(current_url, head)
-        all_data.extend(response_data['data'])  # Assuming each page has a 'data' key with the required data
-
-        next_page_url = response_data.get('paging', {}).get('next')
-        
-        # If there are date filters, update the current_url with them
-        if filters:
-            current_url = f"{next_page_url}&{filters}"
-        else:
-            current_url = next_page_url
+    size = 200  # The number of items you want per page
+    page = 1  # Start with the first page
     
+    if filters:
+        current_url = f"{initial_url}?page={page}&size={size}&navigation=True&{filters}"
+    else:
+        current_url = f"{initial_url}?page={page}&size={size}&navigation=True"
+    
+    # Get the first page to retrieve the total count
+    response_data = get_data_from_page(current_url, head)
+    total_items = response_data['paging']['total']
+    total_pages = (total_items + size - 1) // size  # Calculate total number of pages
+    
+    # Now iterate through all pages and collect the data
+    for page in range(1, total_pages + 1):
+        if filters:
+            current_url = f"{initial_url}?page={page}&size={size}&navigation=True&{filters}"
+        else:
+            current_url = f"{initial_url}?page={page}&size={size}&navigation=True"
+        
+        response_data = get_data_from_page(current_url, head)
+        all_data.extend(response_data['data'])  
 
     return all_data
 
@@ -40,9 +46,10 @@ def get_all_pages_data(initial_url, head, filters=None):
 app = FastAPI()
 
 @app.get('/requerir/')
-async def get_item(key: str, chamada: str,
+async def get_item(key: str,
+                   chamada: str,
                    filters: Optional[str] = None):
-    if key == "f0d318283lmzanc81234asd234":
+    if key == 'f0d318283lmzanc81234asd234':
         initial_request_url = f"{BASE_URL}{chamada}"
         all_pages_data = get_all_pages_data(initial_url=initial_request_url, 
                                         head={"Authorization": f"Basic {os.environ.get('AUTH_HEADER')}"},
